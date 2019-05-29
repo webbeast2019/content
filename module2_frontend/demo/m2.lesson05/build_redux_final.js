3662a98da62cf0af1666fa6c534e00e195d0d53d
yuvalbl <yuval.bar.levi@gmail.com>
2019-05-29 20:38:25 +0300

    module 2

--- module2_frontend/demo/m2.lesson05/build_redux_final.js
+++ module2_frontend/demo/m2.lesson05/build_redux_final.js
@@ -1,13 +1,7 @@
-// task 4: remove initial state from createStore:
-// createStore(itemsReducer, initState) => createStore(itemsReducer);
-// initState should still be implemented
+// task 2: add subscribers
+// store methods: getState, dispatch, subscribe
+// actions: add item, clear, select, delete by index
 'use strict';
-
-// store initial state
-const initState = {
-  items: [],
-  selected: -1
-};
 
 // store creator - we can still provide initial state
 function createStore(reducer, initState) {
@@ -36,8 +30,15 @@ function createStore(reducer, initState) {
   };
 }
 
+
+// items initial state
+const itemsInitState = {
+  items: ['Tea cup', 'Coffee mug'],
+  selected: -1
+};
+
 // our only reducer
-function itemsReducer(state, action) {
+function itemsReducer(state = itemsInitState, action) {
   switch (action.type) {
     case "ADD_ITEM":
       return Object.assign({}, state, {
@@ -45,10 +46,10 @@ function itemsReducer(state, action) {
       });
 
     case "CLEAR_ALL_ITEMS":
-      return Object.assign({}, state, {items: []});
+      return Object.assign({}, state, { items: [] });
 
     case "SELECT_ITEM":
-      return Object.assign({}, state, {selected: action.payload});
+      return Object.assign({}, state, { selected: action.payload });
 
     case "DELETE_SELECTED_ITEM":
       return Object.assign({}, state, {
@@ -61,24 +62,3 @@ function itemsReducer(state, action) {
   }
 }
 
-const myStore = createStore(itemsReducer, initState);
-
-// tests
-(function () {
-  const tests = [
-    {description: 'notify on store change', pass: false, state: null},
-  ];
-
-  myStore.subscribe((state, action) => {
-    console.log(`state has change because of action ${action.type}. new state: `, state);
-    tests[0].state = state;
-    tests[0].pass = true;
-  });
-
-  myStore.dispatch({
-    type: "ADD_ITEM",
-    payload: 'Tea cup'
-  });
-
-  tests.forEach((t, index) => console.log(`test ${index} pass: ${t.pass}`, t));
-})();
