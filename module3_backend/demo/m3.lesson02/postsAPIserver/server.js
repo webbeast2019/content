b47a46884a8be10ab69a63fecc9df5ea7fd81832
yuvalbl <yuval.bar.levi@gmail.com>
2019-07-24 20:59:40 +0300

    module 3

--- module3_backend/demo/m3.lesson02/postsAPIserver/server.js
+++ module3_backend/demo/m3.lesson02/postsAPIserver/server.js
@@ -2,19 +2,21 @@ const db = require('./db');
 const express = require('express');
 const app = express();
 
-
 app.get('/about', (req, res) => res.send('About page'));
 
 app.get('/posts/', (req, res) => {
-  res.send(JSON.stringify(db))
+  res.send(db)
 });
 
 app.get('/posts/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const post = db.find(post => post.id === id);
-  res.send(JSON.stringify(post));
+  res.send(post);
 });
 
+app.all('*', (req, res) => {
+  req.status(404).end()
+});
 
 app.listen(3000,
   () => console.log('Post API server listening on port 3000!')
